977d7cad89d0784c42a126a4035879d7ba553054
Olivia Ellis <oaksellis@gmail.com>
2019-01-28 10:16:32 -0500

    configured Phaser project, build levels, adding game mechanics:

--- js/level2.js
+++ js/level2.js
@@ -4,38 +4,88 @@ var level2 = function(game){
 level2.prototype = {
 
     create: function () {
+      // initialize physics engine and stage
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.world.setBounds(0, 0, 800, 600);
         this.game.stage.backgroundColor = '#000000';
+
+      // create Line sprite
+        line1 = this.game.add.sprite(100, 0, 'whiteline');
+        line2 = this.game.add.sprite(100, 300, 'whiteline');
+        this.game.physics.arcade.enable(line1);
+        this.game.physics.arcade.enable(line2);
+
+      // Manual next stage key
         cursors = this.game.input.keyboard.createCursorKeys();
-        line = this.game.add.sprite(100, 0, 'whiteline');
-        this.game.physics.arcade.enable(line);
         var wkey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
         wkey.onDown.addOnce(this.nextLevel, this);
+
+      // Configure enemies
+        this.enemies = this.game.add.group();
+        this.enemies.enableBody = true;
+        this.initEnemies();
+        this.timer = this.game.time.events.loop(500, this.addEnemy, this);
+
+      // Set up RNG
+        var seed = Date.now();
+        this.random = new Phaser.RandomDataGenerator([seed]);
     },
 
-    update: function () {
-      line.body.velocity.x = 0;
-      line.top = 0;
+    initEnemies: function(){
+      // Create 5 enemies with speed -300 (toward left of screen)
+        for(var i = 0; i <= 5; i++){
+            var sprite = this.enemies.create(800, 100, 'whiteballs', null, false);
+            this.game.physics.arcade.enable(sprite);
+            sprite.enableBody = true;
+            sprite.body.velocity.x = -300;
+        }
+    },
 
-      if (cursors.up.isDown) {
-        line.bottom = 300;
+    addEnemy: function(){
+      // 50% chance of spawning an enemy every 0.5 seconds
+        if(Phaser.Utils.chanceRoll(50)){
+          // Phaser function that reuses "killed" sprites
+            var enemy = this.enemies.getFirstDead();
+          // Calculate random spawn height
+            var y = this.random.integerInRange(10, this.game.world.height - 10);
+            enemy.reset(800, y);
+            enemy.body.velocity.x = this.random.integerInRange(-200, -500);
+          // Kill enemies when they exit screen
+            enemy.checkWorldBounds = true;
+            enemy.outOfBoundsKill = true;
+        }
+
+      },
+
+    update: function () {
+      line1.body.velocity.x = 0;
+      line2.body.velocity.x = 0;
+
+      if (cursors.up.isDown && line2.bottom > 300) {
+        line2.bottom = line2.bottom - 1;
+      } else if (line2.bottom < 600) {
+        line2.bottom = line2.bottom + 1;
       }
 
-      if (cursors.down.isDown) {
-        line.top = 300;
+      if (cursors.down.isDown && line1.top < 300) {
+        line1.top = line1.top + 1;
+      } else if (line1.top > 0) {
+        line1.top = line1.top - 1;
       }
 
       if (cursors.left.isDown) {
-        line.body.velocity.x = -150;
+        line1.body.velocity.x = -150;
+        line2.body.velocity.x = -150;
       } else if (cursors.right.isDown) {
-        line.body.velocity.x = 150;
+        line1.body.velocity.x = 150;
+        line2.body.velocity.x = 150;
       }
     },
 
     render: function () {
-      this.game.debug.spriteInfo(line, 32, 32);
-    },
+        this.game.debug.spriteInfo(line1, 32, 32);
+        this.game.debug.spriteInfo(line2, 32, 600-32);
+      },
 
     nextLevel: function(){
       this.game.state.start("level3");
--- js/load.js
+++ js/load.js
@@ -3,9 +3,11 @@ var loadState = function(game){}
 loadState.prototype = {
   preload: function () {
     var loadingLabel = this.game.add.text(80, 150, 'loading...');
-    this.game.load.image('line', 'assets/line.png');
+    this.game.load.image('line1', 'assets/line.png');
+    this.game.load.image('line2', 'assets/line.png');
     this.game.load.image('whiteline', 'assets/whiteline.png');
     this.game.load.image('balls', 'assets/balls.png');
+    this.game.load.image('whiteballs', 'assets/whiteball.png');
     this.game.load.image('line-segment', 'assets/line-segment.png');
   },
 
